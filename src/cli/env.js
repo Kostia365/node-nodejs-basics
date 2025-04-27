87535c3eb67c071ff3a93849fa767816e5e5a928
Kostia365 <90925004+Kostia365@users.noreply.github.com>
2025-04-27 13:05:30 +0200

    2nd part

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    
+    for (let i = 0; i < args.length; i++) {
+        if (args[i].startsWith('RSS_')) {
+            const [key, value] = args[i].split('=');
+            console.log(`${key} : ${value}`); 
+           }
+        }
 };
 
-parseEnv();
+parseEnv()
